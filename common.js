8b7c7c88c8bf725d19771bf37abebbd9207a3cc2
harttle <yangjvn@126.com>
2018-07-27 19:13:04 +0800

    working demo for appshell

--- common.js
+++ common.js
@@ -2,8 +2,13 @@
  * @file
  * @author
  */
+if (/rt-debug/.test(location.search)) {
+    localStorage.debug = 'app:*';
+}
+var debug = localStorage.debug;
+
 require.config({
-    baseUrl: /rt-debug/.test(location.search) ? 'amd_modules' : '//unpkg.com',
+    baseUrl: debug ? 'amd_modules' : '//unpkg.com',
     waitSeconds: 30
 });
 
@@ -24,17 +29,45 @@ define('@searchfe/assert', function (require) {
     return require('@searchfe/assert/index');
 });
 
+function slideIn(service1, service2, options) {
+    var container = document.querySelector('#sfr-app');
+    var loading = document.createElement('span');
+    loading.innerText = 'Loading...没错，这就是 Loading，时长是 3s';
+
+    service1.stop();
+    container.innerHTML = '';
+    container.appendChild(loading);
+
+    setTimeout(function () {
+        service2.renderTo(container);
+    }, 3000);
+}
+
 require(['ralltiir', 'ralltiir-application', 'ralltiir-application/view/view'], function (rt, Service, View) {
     Service.setBackHtml('<i class="fa fa-arrow-left"></i>');
 
-    rt.services.register('/ralltiir-application-demo/home', {
-        title: 'Ralltiir Application',
+    rt.transitions.register({from: 'shell1', to: 'shell2', impl: slideIn});
+    rt.transitions.register({from: 'shell2', to: 'shell1', impl: slideIn});
+    rt.transitions.register({from: undefined, to: 'shell1'});
+    rt.transitions.register({from: undefined, to: 'shell2'});
+
+    rt.services.register('/ralltiir-application-demo/shell-1', {
+        name: 'shell1',
+        title: 'AppShell Page 1',
         back: '<i class="fa fa-home"></i>'
-    }, Service);
+    }, rt.Service);
+    rt.services.register('/ralltiir-application-demo/shell-2', {
+        name: 'shell2',
+        title: 'AppShell Page 2',
+        back: '<i class="fa fa-home"></i>'
+    }, rt.Service);
     rt.services.register('/ralltiir-application-demo-vue/todolist', {
         title: 'Vue Todo List',
         backendUrl: '/ralltiir-application-demo-vue/todolist'
     }, Service);
+    rt.services.register('/ralltiir-application-demo/home', {
+        title: 'Ralltiir Application',
+    }, rt.Service);
     rt.services.register('/ralltiir-application-demo-react/todolist', {
         title: 'React Todo List',
         backendUrl: '/ralltiir-application-demo-react/todolist'
